0f33d398995f15b6da173faae5263560d6e011a4
kareefardi <kareem.farid@efabless.com>
2023-06-12 13:46:33 +0300

    add ifdef PnR for openframe_project_netlists

--- verilog/rtl/openframe_project_netlists.v
+++ verilog/rtl/openframe_project_netlists.v
@@ -24,4 +24,21 @@
 /*                                                          	*/
 /*--------------------------------------------------------------*/
 
+`ifndef PnR
+`ifdef SIM
+`define USE_POWER_PINS
+`endif
+`endif
 `define OPENFRAME_IO_PADS 44
+
+`ifndef PnR
+`ifdef GL
+    `default_nettype wire
+    // Use behavorial model with gate-level simulation
+    `include "rtl/openframe_project_wrapper.v"
+    `include "gl/picosoc.v"
+`else
+    `include "openframe_project_wrapper.v"
+    `include "picosoc.v"
+`endif
+`endif
